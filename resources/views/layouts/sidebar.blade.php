9338c71d68ed1f42b987353790c5da692d668c8a
Khalid <khalidhassan3011@gmail.com>
2025-04-08 16:35:53 +0600

    add menu in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -36,6 +36,33 @@
        </div>
        <div class="menu-item pt-2.25 pb-px">
         <span class="menu-heading uppercase text-2sm font-medium text-gray-500 ps-[10px] pe-[10px]">
+         API Builders
+        </span>
+       </div>
+       <div class="menu-item">
+        <div class="menu-label border border-transparent gap-[10px] ps-[10px] pe-[10px] py-[6px]" href="" tabindex="0">
+         <span class="menu-icon items-start text-gray-500 dark:text-gray-400 w-[20px]">
+          <i class="ki-filled ki-abstract-8 text-lg">
+          </i>
+         </span>
+         <span class="menu-title text-sm font-medium text-gray-800">
+          Easy Builder
+         </span>
+        </div>
+       </div>
+       <div class="menu-item">
+        <div class="menu-label border border-transparent gap-[10px] ps-[10px] pe-[10px] py-[6px]" href="" tabindex="0">
+         <span class="menu-icon items-start text-gray-500 dark:text-gray-400 w-[20px]">
+          <i class="ki-filled ki-abstract-32 text-lg">
+          </i>
+         </span>
+         <span class="menu-title text-sm font-medium text-gray-800">
+          Advanced Builder
+         </span>
+        </div>
+       </div>
+       <div class="menu-item pt-2.25 pb-px">
+        <span class="menu-heading uppercase text-2sm font-medium text-gray-500 ps-[10px] pe-[10px]">
          User
         </span>
        </div>
