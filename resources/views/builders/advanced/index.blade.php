0e82fe7eb9348b67313523e7b3321c2540eee41f
Khalid <khalidhassan3011@gmail.com>
2025-04-09 12:07:15 +0600

    breadcrumbs

--- resources/views/builders/advanced/index.blade.php
+++ resources/views/builders/advanced/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.main')
 
+@section('breadcrumbs')
+    <span class="text-gray-700">
+        API Builders
+    </span>
+    <i class="ki-filled ki-right text-gray-500 text-3xs">
+    </i>
+    <span class="text-gray-700">
+        Advanced
+    </span>
+@endsection
+
 @section('content')
     advanced builder content here
 @endsection
