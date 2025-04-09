f81f2f42c5730d2c58175fb8c56ee840c02cd935
Khalid <khalidhassan3011@gmail.com>
2025-04-09 12:13:24 +0600

    change format

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::get('/', function () { return view('dashboard');})->name('dashboard');
 
-Route::get('/easy', function () {
-    return view('builders.easy.index');
-})->name('builders.easy.index');
+Route::prefix('builders')->group(function () {
+    Route::get('/easy', function () { return view('builders.easy.index');})->name('builders.easy.index');
 
-Route::get('/advanced', function () {
-    return view('builders.advanced.index');
-})->name('builders.advanced.index');
+    Route::get('/advanced', function () { return view('builders.advanced.index');})->name('builders.advanced.index');
+});
