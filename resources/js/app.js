30b15acbff0f5d91e158f86d2b0851d8f262e2c6
Khalid <khalidhassan3011@gmail.com>
2025-04-10 13:28:13 +0600

    fix padding and import others

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import "./bootstrap";
 
 import "../metronic/core/index";
+
+import "../metronic/app/datatables/allowed-ip-addresses";
+import "../metronic/app/datatables/current-sessions";
 import "../metronic/app/layouts/demo1";
+import "../metronic/app/widgets/general";
 
